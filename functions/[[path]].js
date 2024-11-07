ce87bd903527da9061aac9a0fd6e3c681ce63acb
NMJKj <bfgbrterfweqwer@outlook.dk>
2024-11-07 09:22:11 +0100

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://dfbvsdfs.store'
+	const proxyUrl = 'https://nhgfnghs.store'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
